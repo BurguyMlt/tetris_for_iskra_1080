fba8d7a549b06408479e5aa4594683a71a0f3a5a
Aleksey Morozov <you@example.com>
2018-04-02 03:20:15 +0300

    Черный прямоугольник вокруг GameOver, немного приборки, немного оптимизации

--- tetris.asm
+++ tetris.asm
@@ -1,4 +1,44 @@
+; 2-04-2018 Aleksey Morozov aka Alemorf
+; aleksey.f.morozov@gmail.com
+
+; TETRIS WAS INVENTED BY A 30-YEAR-OLD SOVIET RESEARCHER NAMED ALEXEY PAZHITNOV
+; WHO CURRENTLY WORKS AT THE COMPUTER CENTRE(ACADEMY SOFT) OF THE USSR ACADEMY
+; OF SCIENCES IN MOSCOW. THE ORIGINAL PROGRAMMER WAS 18-YO VADIM GERASIMOV. A
+; STUDENT STUDYING COMPUTER INFORMATICS AT MOSCOW UNIVERSITY. NOW YOU CAN ENJOY
+; TETRIS BECAUSE OF THE JOINT EFFORTS OF ACADEMY SOFT, MOSCOW, ANDROMEDA
+; SOFTWARE LTD, LONDON, ALEMORF, SPB AND SPECTRUM HOLOBYTE, USA.
+
+; VERSION            PROGRAMMER
+; ISKRA 1080 TARTU   ALEKSEY MOROZOV
+; IBM CGA            ENG AN JIO
+; RAM RESIDENT       ERICK JAP
+; TANDY              BILLY SUTYONO
+; IBM EGA            ARYANTO WIDODO
+; GRAPHICS           DAN GUERRA
+; PRODUCT MANAGER    R. ANTON WIDJAJA
+; PRODUCER           SEAN B. BARGER
+
 .i8080
+
+; Карта памяти после загрузки
+; 0100h - 8C00h Код игры
+; 8С00h - 93FEh Сжатая заставка интро заехала в видеопамять
+; 9000h - BFFFh (Видеопамять)
+; C800h - C87Fh (Системные переменные)
+; C880h - C900h (Стек)
+; D000h - FFFFh (Видеопамять)
+
+; Карта памяти после переноса
+; 0003h - 00CAh Кеш игового поля (10x20), временное место хранения системных переменных (80h)
+; 00CBh - 00FFh (Стек)
+; 0100h - 8C00h Код игры
+; 9000h - BFFFh (Видеопамять)
+; С000h - C7FFh Сжатая заставка интро, изображения для игры
+; C800h - C87Fh (Системные переменные)
+; C880h - CFFFh Изображения для интро
+; D000h - FFFFh (Видеопамять)
+
+;----------------------------------------------------------------------------------------------------------------------
 
 org 234
 fileStart:
@@ -6,6 +46,8 @@ fileStart:
 .dw entry
 .dw end1
 .dw entry
+
+OPCODE_MVI_M = 036h
 
 ;----------------------------------------------------------------------------------------------------------------------
 
@@ -24,8 +66,17 @@ entry:
 restartGame:
     call tetris
 
+    ; Очистить прямоугольник
+    lxi  h, OPCODE_MVI_M
+    call copyImageTo
+    lxi  h, 0FFFFh - 135 - (32 * 256)
+    lxi  b, 18 * 256 + 30 ; width * 256 + height
+    lxi  d, 0
+    call copyImage
+
     ; Выводим надпись "GAME OVER"
-    call setTextColorNt
+    mvi  a, 2
+    call setTextColor
     lxi  h, gameOverText
     call drawText
 
@@ -39,8 +90,10 @@ entry_1:
     ;
     jmp  restartGame
 
+;----------------------------------------------------------------------------------------------------------------------
+
 gameOverText:
-    db 9,19," GAME OVER ",0
+    db 9,20,"GAME OVER",0
 
 ;----------------------------------------------------------------------------------------------------------------------
 
@@ -213,11 +266,11 @@ OPCODE_ANA_B = 0A0h
 OPCODE_ORA_B = 0B0h
 OPCODE_XRA_A = 0AFh
 
-setTextColorNt:
-    mvi  a, 1
-    lxi  h, OPCODE_NOP + (OPCODE_NOP * 256)
-    lxi  d, OPCODE_XRA_A + (OPCODE_NOP * 256)
-    jmp  setTextColor1
+;setTextColorNt:
+;    mvi  a, 1
+;    lxi  h, OPCODE_NOP + (OPCODE_NOP * 256)
+;    lxi  d, OPCODE_XRA_A + (OPCODE_NOP * 256)
+;    jmp  setTextColor1
 
 ;----------------------------------------------------------------------------------------------------------------------
 
